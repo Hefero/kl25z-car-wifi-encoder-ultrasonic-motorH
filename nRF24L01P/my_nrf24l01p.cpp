fca6600e4f3db7f1883b2ca4228ae3c5ae5145bd
Hefero <guilherme.marques00@hotmail.com>
2023-09-20 16:16:05 +0000

    indent

--- nRF24L01P/my_nrf24l01p.cpp
+++ nRF24L01P/my_nrf24l01p.cpp
@@ -10,24 +10,17 @@
 #define RX_ADDRESS 12344321
 
 
-my_nrf24l01p::my_nrf24l01p(void):_my_nrf24l01p(PTD2, PTD3, PTC5, PTD0, PTD5, PTA13),_serial(USBTX, USBRX){
-    
+my_nrf24l01p::my_nrf24l01p(void):_my_nrf24l01p(PTD2, PTD3, PTC5, PTD0, PTD5, PTA13),_serial(USBTX, USBRX){    
     _serial.printf( "Initializing Receiver\r\n");
-    
-
     _my_nrf24l01p.powerUp();
-    
     _my_nrf24l01p.setTransferSize(TRANSFER_SIZE);
-    
     _my_nrf24l01p.setReceiveMode();
     _my_nrf24l01p.setRxAddress(RX_ADDRESS);
     _my_nrf24l01p.enable();
-
     // Display the (default) setup of the nRF24L01+ chip
     _serial.printf( "nRF24L01+ Frequency    : %d MHz\r\n",  _my_nrf24l01p.getRfFrequency() );
     _serial.printf( "nRF24L01+ Output power : %d dBm\r\n",  _my_nrf24l01p.getRfOutputPower() );
     _serial.printf( "nRF24L01+ Data Rate    : %d kbps\r\n", _my_nrf24l01p.getAirDataRate() );
-
     _serial.printf( "Type keys to test transfers:\r\n  (transfers are grouped into %d characters)\r\n", TRANSFER_SIZE );
 }
 
