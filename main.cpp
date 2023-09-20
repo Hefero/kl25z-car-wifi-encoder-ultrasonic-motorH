cf1201a76dccba1c3c513b976d70f3fe41186028
Hefero <guilherme.marques00@hotmail.com>
2023-09-20 07:23:20 +0000

    messageTickers

--- main.cpp
+++ main.cpp
@@ -3,29 +3,32 @@
 #include "MotorH.h"
 
 #define DEBUG_MSG_INTERVAL 5.0
+#define HAS_MSG_INTERVAL 0.01
 #define MAX_EXECUTION_TIME 60.0
 
-Serial pc(USBTX, USBRX); // tx, rx
+Serial pc(USBTX, USBRX);
 
 motorH *motor = new motorH(); // classe de controle do motor
 
 my_nrf24l01p *nrf24l01p = new my_nrf24l01p(); // classe de controle do receptor wifi
 
-DigitalOut greenLED(LED_GREEN);
-DigitalOut redLED(LED_RED);
 Ticker debugTicker;
-Timeout executionTimeout;
+Ticker messageTicker;
+Timeout executionTicker;
+
+void hasMessageTicker(){
+    if(nrf24l01p->hasMessage){        
+        pc.printf("command received\n\r");
+        motor->execute(nrf24l01p->rxData);
+        nrf24l01p->hasMessage = false;
+    }
+}
 
 int main() {
     motor->stop();
-    debugTicker.attach(callback(motor,&motorH::debug),DEBUG_MSG_INTERVAL); //habilita debugger do Motor
-    
+    debugTicker.attach(callback(motor,&motorH::debug),DEBUG_MSG_INTERVAL); //habilita debugger do Motor    
+    messageTicker.attach(callback(nrf24l01p,&my_nrf24l01p::checkMessage),HAS_MSG_INTERVAL);    
     while (1) {
-        nrf24l01p->checkMessage();
-        if(nrf24l01p->hasMessage){
-            //executionTimeout.attach(callback(motor,&motorH::execute(nrf24l01p->rxData)),MAX_EXECUTION_TIME);
-            motor->execute(nrf24l01p->rxData);
-            nrf24l01p->hasMessage = false;
-        }
+        hasMessageTicker();
     }
 }
